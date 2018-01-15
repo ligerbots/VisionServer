ca4983d300e45fda726853760bdbc5a5df6791d7
David <David@192.168.0.103>
2018-01-15 12:29:05 -0500

    create cubefinder2018 class

---
+++ server/cubefinder2018.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy
+import argparse
+
+class cubefinder2018(object):
+    '''Find power cube for PowerUp 2018'''
+    
+    #init:
+    
+    #methods:
