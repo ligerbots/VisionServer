6b25d3a7b148e70bb8f364037c238588ea10f8b8
Paul Rensing <prensing@ligerbots.org>
2019-01-18 20:26:28 -0500

    Add new utility programs
    Add a couple new utility programs:
    - camera_settings.py - copy of "settings.py" pulled directly from the RobotPy project
        handy to check/change the settings on a camera without an additional tool
    - opencv_capture.py - simple script to display the image
        will only with with an attached screen, so really only for debugging

---
+++ utils/opencv_capture.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import cv2
+
+dev = int(sys.argv[1])
+cap = cv2.VideoCapture(dev)
+
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)
+print('auto exposure=', cap.get(cv2.CAP_PROP_AUTO_EXPOSURE))
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 60)
+# cap.set(cv2.CAP_PROP_BACKLIGHT, 1)
+
+print('size', cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print('backlight', cap.get(cv2.CAP_PROP_BACKLIGHT))
+print('fps=', cap.get(cv2.CAP_PROP_FPS))
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        print('error capturing image ret=', ret)
+        sys.exit(1)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
