0c3168686110c6f674063258afba5b9b03de2fd9
Paul Rensing <prensing@ligerbots.org>
2020-04-11 15:11:59 -0400

    Add documentation on the tuning parameters of hough_fit
    Thanks to DavidG's use of this for another project.

--- server/hough_fit.py
+++ server/hough_fit.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
-# Use the Hough line algorithm to fix a contour
+# Use the Hough line algorithm to fit a contour
+
+# This algorithm works by using cv2.HoughLines() to find candidate straight lines
+#  in a provided contour. After that it will use one of two different methods
+#  to find the "best" lines to use.
+# 1) if given an approx. fit which closely matches the best answer,
+#    it will find the set of hough lines which most closely matches that fit.
+# or 2) it will find the best N lines where the intersections are close to the
+#    bounding box of the input shape.
 
 # See bottom of this file for some timing results
 
@@ -8,18 +16,49 @@ import cv2
 
 # it is faster if you import the needed functions directly
 # these are used a lot, so it helps, a little
-from math import sin, cos, atan2, degrees
+from math import sin, cos, atan2, degrees, radians
 from numpy import array, zeros, pi, sqrt, uint8
 
-from codetimer import CodeTimer
 two_pi = 2.0 * pi
 pi_by_2 = pi / 2.0
 
+# Explanation of tuning parameters:
+#
+# hough_threshold is the threshold used in the call to HoughLines. It is essentially
+#  the number of points on the input contour which lie along the given line.
+#  This should be tuned based on the size/resolution of the image and length of lines.
+#  Too low, you won't find all the lines; too high wastes time.
+#
+# boundbox_padding is used in _find_sides(). Look for intersections which are close to the bounding
+#  rectangle of the original contour. This parameter defines how many pixels
+#  (in x and y separately) the intersection may be outside the bounding rect.
+#  If the desired corners are actually well outside the input contour (e.g. fitting
+#  a rectangle to a truncated shape), this needs to be larger.
+#
+# match_lines_rho_thres, match_lines_theta_thres
+#  These 2 thresholds are used in _match_lines_to_fit(). They set the upper
+#  thresholds (in rho and theta) on the difference between a line from the
+#  input approximate fit and each Hough line. If the approx. fit contour is
+#  pretty good, then these can be tight; if bad, they need to be loosened.
+#  Also, the rho threshold is in pixels, so if the image resolution is higher,
+#  then the threshold should be scaled up.
 
-def hough_fit(contour, nsides=None, approx_fit=None, image_frame=None):
+
+def hough_fit(contour, nsides=None, approx_fit=None, image_frame=None,
+              hough_threshold=8,
+              boundbox_padding=5,
+              match_lines_rho_thres=10.0,
+              match_lines_theta_thres=radians(20.0)):
     '''Use the Hough line finding algorithm to find a polygon for contour.
     It is faster if you can provide an decent initial fit - see approxPolyDP_adaptive().
-    Pass in image_frame to see the lines found from HoughLines (use for debug only).'''
+    Pass in image_frame to see the lines found from HoughLines (use for debug only).
+
+    Tuning parameters:
+    hough_threshold - voting threshold passed to HoughLines(). Min. number of contour points on a line
+    boundbox_padding - padding (in pixels) when testing if an intersection is close to the bounding box
+    match_lines_rho_thres - allowed distance (in pixels) between a hough line and a approx_fit side
+    match_lines_theta_thres - allowed angle (radians) between a hough line and a approx_fit side
+    '''
 
     if approx_fit is not None:
         nsides = len(approx_fit)
@@ -32,10 +71,9 @@ def hough_fit(contour, nsides=None, approx_fit=None, image_frame=None):
     shifted_con = contour - offset_vec
 
     # the binning does affect the speed, so tune it....
-    with CodeTimer("HoughLines"):
-        contour_plot = zeros(shape=(h, w), dtype=uint8)
-        cv2.drawContours(contour_plot, [shifted_con, ], -1, 255, 1)
-        lines = cv2.HoughLines(contour_plot, 1, pi / 180, threshold=8)
+    contour_plot = zeros(shape=(h, w), dtype=uint8)
+    cv2.drawContours(contour_plot, [shifted_con, ], -1, 255, 1)
+    lines = cv2.HoughLines(contour_plot, 1, pi / 180, threshold=hough_threshold)
 
     if image_frame is not None:
         print('hough lines:')
@@ -50,9 +88,9 @@ def hough_fit(contour, nsides=None, approx_fit=None, image_frame=None):
         return None
 
     if approx_fit is not None:
-        res = _match_lines_to_fit(approx_fit - offset_vec, lines, w, h)
+        res = _match_lines_to_fit(approx_fit - offset_vec, lines, w, h, match_lines_rho_thres, match_lines_theta_thres)
     else:
-        res = _find_sides(nsides, lines, w, h)
+        res = _find_sides(nsides, lines, w, h, boundbox_padding)
 
     if res is None:
         return None
@@ -99,16 +137,21 @@ def plot_hough_line(frame, rho, theta, color=(0, 0, 255), thickness=1, offset=No
 # Private routines
 
 
-def _find_sides(nsides, hough_lines, w, h):
+def _find_sides(nsides, hough_lines, w, h, boundbox_padding):
     # The returned lines from HoughLines() are ordered by confidence, but there may/will be
     #  many variants of the best lines. Loop through the lines and pick the best from
     #  each cluster.
 
     contour_center = (w / 2, h / 2)
-    boundaries = (-5, w+5, -5, h+5)
+    boundaries = (-boundbox_padding, w+boundbox_padding, -boundbox_padding, h+boundbox_padding)
 
-    dist_thres = 10
-    theta_thres = pi / 36  # 5 degrees
+    # these are thresholds on how different a candidate must be from
+    # previously found lines. Essentially any lines below these thresholds
+    # is deemed to be the same as previous ones found, so discard.
+    # Probably don't need to tune these unless you have a very strange target shape.
+    rho_thres = 10.0
+    theta_thres = pi / 36.0  # 5 degrees
+
     best_lines = []
     for linelist in hough_lines:
         line = linelist[0]
@@ -118,8 +161,8 @@ def _find_sides(nsides, hough_lines, w, h):
 
         coord_near_ref = _compute_line_near_reference(line, contour_center)
 
-        if not best_lines or not _is_close(best_lines, line, coord_near_ref, dist_thres, theta_thres):
-            # print('best line:', line[0], math.degrees(line[1]))
+        if not best_lines or not _is_close(best_lines, line, coord_near_ref, rho_thres, theta_thres):
+            # print('best line:', line[0], degrees(line[1]))
             best_lines.append((line, coord_near_ref))
 
         if len(best_lines) == nsides:
@@ -178,11 +221,10 @@ def _delta_angle(a, b):
     return (d + pi) % two_pi - pi
 
 
-def _match_lines_to_fit(approx_fit, hough_lines, w, h):
+def _match_lines_to_fit(approx_fit, hough_lines, w, h, rho_thres, theta_thres):
     '''Given the approximate shape and a set of lines from the Hough algorithm
     find the matching lines and rebuild the fit'''
 
-    theta_thres = pi / 9  # 20 degrees
     nsides = len(approx_fit)
     fit_sides = []
     hough_used = set()
@@ -204,8 +246,8 @@ def _match_lines_to_fit(approx_fit, hough_lines, w, h):
             # There is an ambiguity of -rho and adding 180deg to theta
             # So test them both.
 
-            if (abs(rho - line[0]) < 10 and abs(_delta_angle(theta, line[1])) < theta_thres) or \
-               (abs(rho + line[0]) < 10 and abs(_delta_angle(theta, line[1] - pi)) < theta_thres):
+            if (abs(rho - line[0]) < rho_thres and abs(_delta_angle(theta, line[1])) < theta_thres) or \
+               (abs(rho + line[0]) < rho_thres and abs(_delta_angle(theta, line[1] - pi)) < theta_thres):
                 fit_sides.append(line)
                 hough_used.add(ih)
                 # print('  matched:', ih, line[0], degrees(line[1]))
@@ -228,44 +270,43 @@ def _match_lines_to_fit(approx_fit, hough_lines, w, h):
 
 
 def _compute_line_near_reference(line, ref_point):
-    with CodeTimer("compute_line_near_reference"):
-        rho, theta = line
+    rho, theta = line
 
-        # remember: theta is actually perpendicular to the line, so there is a sign flip
-        cos_theta = cos(theta)
-        sin_theta = sin(theta)
-        x0 = cos_theta * rho
-        y0 = sin_theta * rho
+    # remember: theta is actually perpendicular to the line, so there is a sign flip
+    cos_theta = cos(theta)
+    sin_theta = sin(theta)
+    x0 = cos_theta * rho
+    y0 = sin_theta * rho
 
-        if abs(cos_theta) < 1e-6:
-            x_near_ref = None
-            y_near_ref = y0
-        elif abs(sin_theta) < 1e-6:
-            x_near_ref = x0
-            y_near_ref = None
-        else:
-            x_near_ref = x0 + (y0 - ref_point[1]) * sin_theta / cos_theta
-            y_near_ref = y0 + (x0 - ref_point[0]) * cos_theta / sin_theta
+    if abs(cos_theta) < 1e-6:
+        x_near_ref = None
+        y_near_ref = y0
+    elif abs(sin_theta) < 1e-6:
+        x_near_ref = x0
+        y_near_ref = None
+    else:
+        x_near_ref = x0 + (y0 - ref_point[1]) * sin_theta / cos_theta
+        y_near_ref = y0 + (x0 - ref_point[0]) * cos_theta / sin_theta
 
     return x_near_ref, y_near_ref
 
 
-def _is_close(best_lines, candidate, coord_near_ref, dist_thres, theta_thres):
+def _is_close(best_lines, candidate, coord_near_ref, rho_thres, theta_thres):
     cand_rho, cand_theta = candidate
 
-    # print('cand:', cand_rho, math.degrees(cand_theta))
+    # print('cand:', cand_rho, degrees(cand_theta))
     for line in best_lines:
         line, best_near_ref = line
         # print('best', line, best_near_ref)
 
-        delta_dists = []
+        delta_rhos = []
         if coord_near_ref[0] is not None and best_near_ref[0] is not None:
-            delta_dists.append(abs(coord_near_ref[0] - best_near_ref[0]))
+            delta_rhos.append(abs(coord_near_ref[0] - best_near_ref[0]))
         if coord_near_ref[1] is not None and best_near_ref[1] is not None:
-            delta_dists.append(abs(coord_near_ref[1] - best_near_ref[1]))
-        if not delta_dists:
+            delta_rhos.append(abs(coord_near_ref[1] - best_near_ref[1]))
+        if not delta_rhos:
             return True
-        delta_dist = min(delta_dists)
+        delta_rho = min(delta_rhos)
 
         # angle differences greater than 180deg are not real
         delta_theta = cand_theta - line[1]
@@ -275,8 +316,8 @@ def _is_close(best_lines, candidate, coord_near_ref, dist_thres, theta_thres):
             delta_theta += pi
         delta_theta = abs(delta_theta)
 
-        # print('test:', line[0], math.degrees(line[1]), delta_dist, delta_theta)
-        if delta_dist <= dist_thres and delta_theta <= theta_thres:
+        # print('test:', line[0], degrees(line[1]), delta_rho, delta_theta)
+        if delta_rho <= rho_thres and delta_theta <= theta_thres:
             return True
     return False
 
@@ -288,22 +329,21 @@ def _intersection(line1, line2):
     See https://stackoverflow.com/a/383527/5087436
     """
 
-    with CodeTimer("intersection"):
-        rho1, theta1 = line1
-        rho2, theta2 = line2
-        if abs(theta1 - theta2) < 1e-6:
-            # parallel
-            return None
+    rho1, theta1 = line1
+    rho2, theta2 = line2
+    if abs(theta1 - theta2) < 1e-6:
+        # parallel
+        return None
 
-        cos1 = cos(theta1)
-        sin1 = sin(theta1)
-        cos2 = cos(theta2)
-        sin2 = sin(theta2)
+    cos1 = cos(theta1)
+    sin1 = sin(theta1)
+    cos2 = cos(theta2)
+    sin2 = sin(theta2)
 
-        denom = cos1*sin2 - sin1*cos2
-        x = (sin2*rho1 - sin1*rho2) / denom
-        y = (cos1*rho2 - cos2*rho1) / denom
-        res = array((x, y))
+    denom = cos1*sin2 - sin1*cos2
+    x = (sin2*rho1 - sin1*rho2) / denom
+    y = (cos1*rho2 - cos2*rho1) / denom
+    res = array((x, y))
     return res
 
 
